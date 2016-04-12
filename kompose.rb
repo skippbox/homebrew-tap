3c63d74e82c4d8905c343fdd309e08a93454d247
runseb <runseb@gmail.com>
2016-04-12 17:07:17 +0200

    Update kompose.rb

--- kompose.rb
+++ kompose.rb
@@ -4,11 +4,11 @@ class Kompose < Formula
   version "0.0.3"
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/skippbox/kompose/releases/download/v0.0.3.alpha.2/kompose_darwin-amd64.tar.gz"
-    sha256 "6567fe2d8addd90f4e74ceafaa27918378ee94ac495664b404600d6a8750764a"
+    url "https://github.com/skippbox/kompose/releases/download/v0.0.3/kompose_darwin-amd64.tar.gz"
+    sha256 "23234a3239286532150b81f27d660febf7250251376b3348af273062d93a2d45"
   else
-    url "https://github.com/skippbox/kompose/releases/download/v0.0.3.alpha.2/kompose_darwin-386.tar.gz"
-    sha256 "47c732dbd12584cf7b4509de3cb64b6e46eb888e09596b854139792dff9e812b"
+    url "https://github.com/skippbox/kompose/releases/download/v0.0.3/kompose_darwin-386.tar.gz"
+    sha256 "1a00818db3a77dc2d696e45f275e190003e86f39897c1e88ff70d7c66c247b93"
   end
 
   bottle :unneeded
